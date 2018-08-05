d34447cccd4436af9e0fa43755d6e9b513cf783f
Admin <Admin@Admin-PC>
2018-08-05 13:59:22 +0530

    Committing Arithmetic program

---
+++ src/main/java/week2homework/ArithmeticOperators.java
@@ -0,0 +1,16 @@
+package week2homework;
+
+public class ArithmeticOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int firstNumber = 30;
+      int secondNumber= 40;
+      System.out.println(" Addition of two numbers " + " = " + (firstNumber + secondNumber));
+      System.out.println(" Subtraction of two numbers " + " = " +  (firstNumber - secondNumber));
+      System.out.println(" Multiplication of two numbers " + " = " + (firstNumber * secondNumber));
+      System.out.println("Modulo of two numbers " + " = " + (firstNumber%secondNumber));
+     
+	}
+
+}
